fc48efb308d0dd5c98397d1ed64ddc1790ad4f64
parthgupta9 <parth615gupta@gmail.com>
2024-11-06 18:50:09 +0530

    nav

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,14 +3,26 @@ import Link from 'next/link';
 import { DiDropbox } from "react-icons/di";
 
 
+
+
 const Navbar = () => {
+    const links = [
+        {label:'Dashboard',href:'/' },
+        {label:'Issues',href:'/issues'}
+    ]
   return (
     <div>
         <nav className='flex mb-5 space-x-6 border-b border-b-black h-14 items-center px-8'>
             <Link href="/"><p className='w-full h-full'><DiDropbox /> </p></Link>
             <ul className='flex space-x-6'>
-                <li className='text-xl text-zinc-600 hover:text-black font-medium'> <Link href="/Dash">Dashoard</Link></li>
-                <li className='text-xl  text-zinc-600 hover:text-black font-medium'> <Link href="/Issue">Issues</Link></li>
+                {links.map(link =>
+                 <Link
+                  key={link.href}
+                   className='text-xl text-zinc-600 hover:text-black font-medium' 
+                   href={link.href}
+                 >{link.label}</Link>
+                   
+                )}
             </ul>
         </nav>
     </div>
